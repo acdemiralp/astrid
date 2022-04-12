a378f424c0e1f1b7fe18a0bd4a1c32351deda9a8
acdemiralp <demiralpali@gmail.com>
2022-04-13 01:14:17 +0200

    Implementation progress.

--- source/service.cpp
+++ source/service.cpp
@@ -13,28 +13,39 @@ namespace ast
 {
 std::int32_t service::run(std::int32_t argc, char** argv) 
 {
-  const std::string address = "tcp://*:3000"; // TODO: Retrieve port from arguments.
+  mpi::environment  environment ;
+  mpi::communicator communicator;
 
-  zmq::context_t context(1);
-  zmq::socket_t  socket (context, ZMQ_PAIR);
-  socket.bind(address);
-  std::cout << "Service running at: " << address << "\n";
+  zmq::context_t    context(1);
+  zmq::socket_t     socket (context, ZMQ_PAIR);
+  if (communicator.rank() == 0)
+  {
+    const std::string address = "tcp://*:3000"; // TODO: Retrieve port from arguments.
+    socket.bind(address);
+    std::cout << "Service running at: " << address << "\n";
+  }
   
   ::request request;
   ::image   image  ;
 
   while (true)
   {
-    zmq::message_t received;
-    auto recv_result = socket.recv(received, zmq::recv_flags::none);
-    request.ParseFromArray(received.data(), static_cast<std::int32_t>(received.size()));
+    if (communicator.rank() == 0)
+    {
+      zmq::message_t received;
+      auto recv_result = socket.recv(received, zmq::recv_flags::none);
+      request.ParseFromArray(received.data(), static_cast<std::int32_t>(received.size()));
+    }
 
-    // TODO: Create the image based on the request.
+    // TODO: Create the image based on the request. Propagate the request to other ranks.
 
-    auto string      = image.SerializeAsString();
-    zmq::message_t sent(string.begin(), string.end());
-    auto send_result = socket.send(sent, zmq::send_flags::none);
-    std::cout << "Sent image with size: " << image.size().x() << " " << image.size().y() << "\n";
+    if (communicator.rank() == 0)
+    {
+      auto string      = image.SerializeAsString();
+      zmq::message_t sent(string.begin(), string.end());
+      auto send_result = socket.send(sent, zmq::send_flags::none);
+      std::cout << "Sent image with size: " << image.size().x() << " " << image.size().y() << "\n";
+    }
   }
 
   return 0;

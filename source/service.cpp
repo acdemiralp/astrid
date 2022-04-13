aca69dc02a0173ad51721f1dedfd3a8a658d4677
acdemiralp <demiralpali@gmail.com>
2022-04-13 02:13:43 +0200

    Progress.

--- source/service.cpp
+++ source/service.cpp
@@ -26,24 +26,39 @@ std::int32_t service::run(std::int32_t argc, char** argv)
   }
   
   ::request request;
-  ::image   image  ;
-
   while (!request.terminate())
   {
+    std::int32_t              size;
+    std::vector<std::uint8_t> data;
+
     if (communicator.rank() == 0)
     {
-      zmq::message_t received;
-      auto recv_result = socket.recv(received, zmq::recv_flags::none);
-      request.ParseFromArray(received.data(), static_cast<std::int32_t>(received.size()));
+      zmq::message_t message;
+      socket.recv(message, zmq::recv_flags::none);
+
+      size = static_cast<std::int32_t>(message.size());
+      data.resize(message.size());
+      std::copy_n(static_cast<std::uint8_t*>(message.data()), message.size(), data.begin());
     }
-
-    // TODO: Propagate the request to other ranks and render the image.
+    
+    communicator.bcast(&size      , 1   , mpi::data_type(MPI_INT ));
+    data.resize(size);
+    communicator.bcast(data.data(), size, mpi::data_type(MPI_BYTE));
+    request.ParseFromArray(data.data(), static_cast<std::int32_t>(data.size()));
+    
+    // TODO: Render the image according to the request.
+    image<float> result;
 
     if (communicator.rank() == 0)
     {
-      auto string      = image.SerializeAsString();
-      zmq::message_t sent(string.begin(), string.end());
-      auto send_result = socket.send(sent, zmq::send_flags::none);
+      ::image image;
+      image.mutable_data()->assign(result.data.begin(), result.data.end());
+      image.mutable_size()->set_x (result.size[0]);
+      image.mutable_size()->set_y (result.size[1]);
+      auto string = image.SerializeAsString();
+
+      zmq::message_t message(string.begin(), string.end());
+      socket.send(message, zmq::send_flags::none);
       std::cout << "Sent image with size: " << image.size().x() << " " << image.size().y() << "\n";
     }
   }

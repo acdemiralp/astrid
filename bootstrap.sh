fec825ecb01f4f85e96c538bc400d594f5cbf761
acdemiralp <demiralpali@gmail.com>
2022-04-20 01:30:49 +0200

    Parallel build. View adjustments. Warning fixes.

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,5 +11,5 @@ vcpkg install --recurse --overlay-ports=../../vcpkg/overlay_ports cppzmq cxxopts
 cd ..
 
 cmake -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake ..
-cmake --build . --target ALL_BUILD --config Release
+cmake --build . --target ALL_BUILD --config Release --parallel 8
 cd ..
